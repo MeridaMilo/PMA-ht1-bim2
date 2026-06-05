8636f0f5110b07a257710203afa20d6ebfc5e430
Emilio <Emilio@DESKTOP-A7F6FQB>
2026-06-05 16:43:13 -0600

    003 se agrega metodo calculo de salario

---
+++ src/src/org/josecastaneda/main/Controllers/CalculoNumericoController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src.org.josecastaneda.main.Controllers;
+
+/**
+ *
+ * @author Emilio
+ */
+public class CalculoNumericoController {
+    
+    public float calculoSalario(float horasTrabajadas,float pagoPorHora){
+        float totalSalario = horasTrabajadas * pagoPorHora;
+        return totalSalario;
+   } 
+}
